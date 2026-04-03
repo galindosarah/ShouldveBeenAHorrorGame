5ab69f37f528fe3f15f128901468bf8542a6e3a6
galindosarah <galindosarah@ufl.edu>
2026-04-03 13:57:23 -0400

    Added quit game script

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -1,23 +1,9 @@
-// Midterm
-// DIG3878
-// Annette Gonzalez
-// This script allows the user to quit the game using the "esc" key.
 using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void QuitGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+        Application.Quit();
     }
 }
