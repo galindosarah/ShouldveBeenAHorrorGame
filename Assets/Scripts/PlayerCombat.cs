bdccd44959de5b02bbf19c48fda3ae62feb100bb
celine-hui <ccelinehui@gmail.com>
2026-04-05 13:11:39 -0400

    Fixed title and credits aspect ratio

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -17,7 +17,7 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetMouseButtonDown(0))
+       if (Input.GetMouseButtonDown(0) && !PauseGame.isPaused)
         {
             Attack();
         }
